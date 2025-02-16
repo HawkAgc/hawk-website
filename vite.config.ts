29872315f320369ea02cda859a5aeeb7cc3d9853
PatrickLeonardo <patricklf7@proton.me>
2025-02-16 16:28:42 -0300

    upload project

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+// @ts-ignore:next-line
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+        tailwindcss(),
+    ],
+    build: {
+        rollupOptions: {
+            input: {
+                main: './index.html'
+            },
+        },
+    },
+})
